10447ea41476dae0617bf401fb2e898c0d4e38d4
Frederick Morrison <frederick.morrison@outlook.com>
2023-11-09 17:01:01 -0700

    upgrade to python 3.12.0

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from rich.prompt import Prompt
 from rich.console import Console
+from rich.color import Color
 from random import choice
 from words import word_list
 
-SQUARES = {
-    'correct_place': '🟩',
-    'correct_letter': '🟨',
-    'incorrect_letter': '⬛'
-}
 
-WELCOME_MESSAGE = f'\n[white on blue] WELCOME TO WORDLE [/]\n'
+SQUARES = {"correct_place": "🟩", "correct_letter": "🟨", "incorrect_letter": "⬛"}
+
+
+WELCOME_MESSAGE = f"\n[white on blue] WELCOME TO WORDLE [/]\n"
 PLAYER_INSTRUCTIONS = "You may start guessing\n"
 GUESS_STATEMENT = "\nEnter your guess"
 ALLOWED_GUESSES = 6
 
-def correct_place(letter):
-    return f'[black on green]{letter}[/]'
+
+def correct_place(letter: str) -> str:
+    return f"[black on green]{letter}[/]"
 
 
-def correct_letter(letter):
-    return f'[black on yellow]{letter}[/]'
+def correct_letter(letter: str) -> str:
+    return f"[black on yellow]{letter}[/]"
 
 
-def incorrect_letter(letter):
-    return f'[black on white]{letter}[/]'
+def incorrect_letter(letter: str) -> str:
+    return f"[black on white]{letter}[/]"
 
 
 def check_guess(guess, answer):
@@ -32,14 +32,14 @@ def check_guess(guess, answer):
     for i, letter in enumerate(guess):
         if answer[i] == guess[i]:
             guessed += correct_place(letter)
-            wordle_pattern.append(SQUARES['correct_place'])
+            wordle_pattern.append(SQUARES["correct_place"])
         elif letter in answer:
             guessed += correct_letter(letter)
-            wordle_pattern.append(SQUARES['correct_letter'])
+            wordle_pattern.append(SQUARES["correct_letter"])
         else:
             guessed += incorrect_letter(letter)
-            wordle_pattern.append(SQUARES['incorrect_letter'])
-    return ''.join(guessed), ''.join(wordle_pattern)
+            wordle_pattern.append(SQUARES["incorrect_letter"])
+    return "".join(guessed), "".join(wordle_pattern)
 
 
 def game(console, chosen_word):
@@ -54,7 +54,7 @@ def game(console, chosen_word):
             if guess in already_guessed:
                 console.print("[red]You've already guessed this word!!\n[/]")
             else:
-                console.print('[red]Please enter a 5-letter word!!\n[/]')
+                console.print("[red]Please enter a 5-letter word!!\n[/]")
             guess = Prompt.ask(GUESS_STATEMENT).upper()
         already_guessed.append(guess)
         guessed, pattern = check_guess(guess, chosen_word)
@@ -64,17 +64,19 @@ def game(console, chosen_word):
         console.print(*all_words_guessed, sep="\n")
         if guess == chosen_word or len(already_guessed) == ALLOWED_GUESSES:
             end_of_game = True
+
     if len(already_guessed) == ALLOWED_GUESSES and guess != chosen_word:
         console.print(f"\n[red]WORDLE X/{ALLOWED_GUESSES}[/]")
-        console.print(f'\n[green]Correct Word: {chosen_word}[/]')
+        console.print(f"\n[green]Correct Word: {chosen_word}[/]")
     else:
         console.print(f"\n[green]WORDLE {len(already_guessed)}/{ALLOWED_GUESSES}[/]\n")
     console.print(*full_wordle_pattern, sep="\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     console = Console()
     chosen_word = choice(word_list)
+    console.print(f"chosen word: {chosen_word}")
     console.print(WELCOME_MESSAGE)
     console.print(PLAYER_INSTRUCTIONS)
     game(console, chosen_word)
